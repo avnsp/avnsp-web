128148964be8a4e5ee3769650e77deb3da4432e7
Magnus Hörberg <magnus.hoerberg@gmail.com>
2013-12-11 14:33:08 +0700

    added forgot password feature

--- controllers/auth.rb
+++ controllers/auth.rb
@@ -2,12 +2,29 @@ require './controllers/base'
 
 class AuthController < BaseController
   before do
-    unless session[:id] || request.path =~ /^\/log(in|out)/
+    unless session[:id] || request.path =~ /^\/log(in|out)|forgot/
       redirect "/login?return_url=#{request.url}"
     end
   end
+
   get '/login' do
     haml :login
+  end
+
+  get '/forgot' do
+    haml :forgot
+  end
+
+  post '/forgot' do
+    member = Member[email: params[:email]]
+    if member
+      flash[:info] = "Ditt nya lösenord skickas till din email"
+      password = member.reset_password
+      publish "member.reset_password", member.to_hash.merge(password: password)
+    else
+      flash[:error] = "Fanns ingen med den emailen, borde det göra det? Skicka ett mail till cdo i sådana fall."
+    end
+    redirect back
   end
   post '/login' do
     @member = Member[nick: params[:nick]]
--- models.rb
+++ models.rb
@@ -1,4 +1,5 @@
 require 'bcrypt'
+require 'securerandom'
 
 class Member < Sequel::Model
   include BCrypt
@@ -9,5 +10,11 @@ class Member < Sequel::Model
   def password=(new_password)
     @password = Password.create(new_password)
     self.password_hash = @password
+  end
+  def reset_password
+    p = SecureRandom.urlsafe_base64(32 * 3/4)
+    self.password = p
+    self.save
+    p
   end
 end
