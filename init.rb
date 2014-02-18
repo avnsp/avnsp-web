0078d69132ae6482aa92f1e3d19f4abef5e40a69
Magnus Hörberg <magnus.hoerberg@gmail.com>
2014-02-18 19:39:50 +0100

    take connection urls from env

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 require 'sequel'
-DB = Sequel.connect 'postgres://localhost/avnsp'
+DB = Sequel.connect ENV['ELEPHANTSQL_URL'] || 'postgres://localhost/avnsp'
 
 require './thumper'
-TH = Thumper::Base.new(publish_to: 'amqp://localhost/avnsp',
-                       consume_from: 'amqp://localhost/avnsp')
+TH = Thumper::Base.new(publish_to: ENV['CLOUDAMQP_URL'] || 'amqp://localhost/avnsp',
+                       consume_from: ENV['CLOUDAMQP_URL'] || 'amqp://localhost/avnsp')
 require 'aws-sdk'
 AWS.config(access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
            secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
