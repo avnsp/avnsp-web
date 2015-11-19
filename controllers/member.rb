d9adcc46c4299b3f4750aadb9003f11cc000c68f
Magnus Hörberg <magnus.hoerberg@gmail.com>
2015-11-19 21:51:20 +0100

    view to edit your profile

--- controllers/member.rb
+++ controllers/member.rb
@@ -1,6 +1,24 @@
 require './controllers/base'
 
 class MemberController < BaseController
+  get '/profile' do
+    haml :profile
+  end
+
+  post '/profile' do
+    m = Member.where(id: @member.id)
+    m.update(first_name: params[:first_name],
+             last_name: params[:last_name],
+             nick: params[:nick],
+             studied: params[:studied],
+             started: params[:started],
+             phone: params[:phone],
+             street: params[:street],
+             zip: params[:zip],
+             city: params[:city])
+    redirect back
+  end
+
   get '/:id' do |id|
     @member = Member[id]
     @parties = @member.parties.sort_by(&:date)
