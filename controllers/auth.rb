bfe76bf574c2af6b693bf5b3742989760d8189dc
Magnus Hörberg <magnus.hoerberg@gmail.com>
2016-01-24 16:22:08 +0100

    make login links expire

--- controllers/auth.rb
+++ controllers/auth.rb
@@ -13,7 +13,7 @@ class AuthController < BaseController
   end
 
   get '/auth' do
-    unless token_valid?(params[:token], params[:email])
+    unless token_valid?(params[:token], params[:email], params[:ts])
       halt 401, 'Ngt blev fel i autensieringen. Skicka ett email till cdo@academian.se'
     end
     session[:id] = Member[email: params[:email]].id
@@ -25,7 +25,8 @@ class AuthController < BaseController
     if @member.nil?
       flash[:info] = 'Den emailen finns inte registrerad.'
     else
-      publish 'member.login', @member.to_hash.merge(token: generate_token(@member.email))
+      dt = DateTime.now
+      publish 'member.login', @member.to_hash.merge(token: generate_token(@member.email, dt))
       flash[:success] = 'Login länken är skickad till din angivna email.'
     end
     redirect back
@@ -37,13 +38,14 @@ class AuthController < BaseController
   end
 
   helpers do
-    def generate_token(email)
-      str = "#{email}:#{ENV['SESSION_SECRET']}"
+    def generate_token(email, ts)
+      str = "#{email}:#{ts}:#{ENV['SESSION_SECRET']}"
       Digest::SHA1.hexdigest(str)
     end
 
-    def token_valid?(token, email)
-      token == generate_token(email)
+    def token_valid?(token, email, params[:ts])
+      dt = DateTime.parse(params[:ts])
+      (dt < (DateTime.now - 300)) && token == generate_token(email, params[:ts])
     end
   end
 end
