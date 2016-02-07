519f17c9abf7edf9bc9993dc14f008b10ae0f205
Magnus Hörberg <magnus.hoerberg@gmail.com>
2016-02-07 20:55:14 +0100

    add invitations handler to email worker

--- workers/email.rb
+++ workers/email.rb
@@ -16,7 +16,20 @@ end
 class EmailWorker
   def start
     subscribe("member.login", "member.login") do |_, msg|
-      send msg[:email], "[Academian] login-länk", haml(:login, msg)
+      s = Struct.new(:email, :token, :ts, :hostname)
+      extras = s.new(msg[:email], msg[:token], msg[:ts], msg[:hostname])
+      send msg[:email], "[Academian] login-länk", haml(:login, extras)
+    end
+
+    subscribe('party.invitation', 'send-invitations') do |_, msg|
+      s = Struct.new(:nick, :party_date, :party_name, :party_last_att_date,
+                     :party_id, :balance, :balance_after, :street, :zip, :city)
+      extras = s.new(msg[:nick], msg[:party_date], msg[:party_name],
+                     msg[:party_last_att_date], msg[:party_id], msg[:balance],
+                     msg[:balance_after], msg[:street], msg[:zip], msg[:city])
+      send(msg[:email],
+           "[Academian] Inbjudan #{msg[:party_name]}",
+           haml(:invitation, extras))
     end
   end
 
@@ -38,7 +51,6 @@ class EmailWorker
   def haml(file_name, extras)
     file = File.read("./emails/#{file_name}.haml")
     engine = Haml::Engine.new(file)
-    s = Struct.new(:email, :token, :ts, :hostname)
-    engine.render(s.new(extras[:email], extras[:token], extras[:ts], extras[:hostname]))
+    engine.render(extras)
   end
 end
