1cec184a6f929cf3bef7b4d66cd9982b9a4a63f0
Magnus Hörberg <magnus.hoerberg@gmail.com>
2016-02-10 11:45:04 +0100

    increase login token valid period to 30 min

--- controllers/auth.rb
+++ controllers/auth.rb
@@ -15,7 +15,7 @@ class AuthController < BaseController
 
   get '/auth' do
     cache_control :public, :must_revalidate, max_age: 0
-    unless (Time.now - 600) < Time.at(params[:ts].to_i)
+    unless (Time.now - 1800) < Time.at(params[:ts].to_i)
       halt 401, 'Den här länken är inte giltig längre, länkarna är giltiga i 10 min. Försök beställa en ny, annars skicka ett email till cdo@academian.se'
     end
     unless token_valid?(params[:token], params[:email], params[:ts])
@@ -51,7 +51,7 @@ class AuthController < BaseController
     end
 
     def token_valid?(token, email, ts)
-      (Time.now - 600) < Time.at(ts.to_i) && token == generate_token(email, ts)
+      (Time.now - 1800) < Time.at(ts.to_i) && token == generate_token(email, ts)
     end
   end
 end
