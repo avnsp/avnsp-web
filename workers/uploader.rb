1c9c5316efaf1e016cdb020701c80ced3f229bcb
Magnus Hörberg <magnus.hoerberg@gmail.com>
2017-11-03 10:59:44 +0100

    disable photo uploads

--- workers/uploader.rb
+++ workers/uploader.rb
@@ -18,29 +18,29 @@ class Uploader
       objects[data[:path]].write(object)
     end
 
-    subscribe 'photo.upload', 'photo.upload' do |rk, data|
-      file = Base64.decode64(data[:file])
-      image = MiniMagick::Image.read(file)
-      data[:versions].each do |v|
-        i = image.dup
-        i.combine_options do |b|
-          b.quality(v[:quality]) if v[:quality]
-          b.resample(v[:resample]) if v[:resample]
-          b.resize(v[:resize]) if v[:resize]
-        end
-
-        ct = i.mime_type
-        objects = @s3.buckets['avnsp'].objects
-        object = {
-          data: i.to_blob,
-          content_type: ct,
-          cache_control: 'max-age=31536000'
-        }
-        objects[v[:path]].write(object)
-      end
-
-      publish 'photo.uploaded', data
-    end
+    #subscribe 'photo.upload', 'photo.upload' do |rk, data|
+      #file = Base64.decode64(data[:file])
+      #image = MiniMagick::Image.read(file)
+      #data[:versions].each do |v|
+        #i = image.dup
+        #i.combine_options do |b|
+          #b.quality(v[:quality]) if v[:quality]
+          #b.resample(v[:resample]) if v[:resample]
+          #b.resize(v[:resize]) if v[:resize]
+        #end
+#
+        #ct = i.mime_type
+        #objects = @s3.buckets['avnsp'].objects
+        #object = {
+          #data: i.to_blob,
+          #content_type: ct,
+          #cache_control: 'max-age=31536000'
+        #}
+        #objects[v[:path]].write(object)
+      #end
+#
+      #publish 'photo.uploaded', data
+    #end
   end
 
   def stop
