ce8a93f6ce8e1041b67359fffed2627f3c1d2066
Magnus Hörberg <magnus.hoerberg@gmail.com>
2026-02-10 12:09:30 +0100

    lint and set FQDN to www.academian.se

--- init.rb
+++ init.rb
@@ -1,21 +1,21 @@
-require 'aws'
+require "aws"
 
-FQDN = 'academian.se'
+FQDN = "www.academian.se"
 
-require 'sequel'
+require "sequel"
 Sequel.extension :core_extensions, :pg_json, :pg_json_ops
 Sequel.split_symbols = true
-DB = Sequel.connect ENV['ELEPHANTSQL_URL'] || 'postgres://localhost/avnsp'
+DB = Sequel.connect ENV["ELEPHANTSQL_URL"] || "postgres://localhost/avnsp"
 
-require './thumper'
-TH = Thumper.new(publish_to: ENV['CLOUDAMQP_URL'] || 'amqp://localhost/avnsp',
-                 consume_from: ENV['CLOUDAMQP_URL'] || 'amqp://localhost/avnsp')
+require "./thumper"
+TH = Thumper.new(publish_to: ENV["CLOUDAMQP_URL"] || "amqp://localhost/avnsp",
+                 consume_from: ENV["CLOUDAMQP_URL"] || "amqp://localhost/avnsp")
 
-require './workers'
+require "./workers"
 TH.register EmailWorker
 TH.register EventWorker
 TH.register Uploader
 
 AWS.config(access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
            secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
-           region: 'eu-west-1')
+           region: "eu-west-1")
