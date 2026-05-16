1379497c274ab992775bbed1761f2ebe5e2dc2d2
ViktorU <viktoruvesten@gmail.com>
2026-05-16 21:32:20 +0200

    Fix uploader PDF test for stubbed S3

--- test/workers/uploader_test.rb
+++ test/workers/uploader_test.rb
@@ -66,9 +66,12 @@ class UploaderTest < Minitest::Test
       path: path
     })
 
-    object = @worker.instance_variable_get(:@bucket).object(path)
-    assert_equal file_data, object.body
-    assert_equal "application/pdf", object.content_type
+    put_request = @worker.instance_variable_get(:@s3).client.api_requests.find do |req|
+      req[:operation_name] == :put_object && req[:params][:key] == path
+    end
+    refute_nil put_request
+    assert_equal file_data, put_request[:params][:body]
+    assert_equal "application/pdf", put_request[:params][:content_type]
     assert @worker.published.any? { |p| p[:topic] == "file.uploaded" }
   end
 end
